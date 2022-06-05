4f14d76e479bf28a7d4d2c5dd47ae860b0dbb6a6
Shazeen Fabius <shazzy.sterlin@gmail.com>
2022-06-05 16:50:31 -0700

    how to use the information in the application.properties and having different profiles

--- src/main/java/com/example/springmasterclass/customer/CustomerConfiguration.java
+++ src/main/java/com/example/springmasterclass/customer/CustomerConfiguration.java
@@ -1,20 +1,31 @@
 package com.example.springmasterclass.customer;
 
+import com.example.springmasterclass.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
     @Value("${app.useFakeRepo:false}")
     private Boolean useFakeRepo;
 
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return arg -> {
+    @Value("${info.company.name}")
+    private String companyName;
 
-            System.out.println("Command Line Runner");
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return arg -> {
+            System.out.println("Command Line Runner ------ ");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.description"));
+            System.out.println(infoApp);
         };
     }
 
